2f18933b84a6e72aa2d70baca9a07f92ce9b14d5
Danik499 <danikshcherban499@gmail.com>
2022-11-02 15:03:55 +0200

    telegram bot and heroku settings added

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use Telegram\Bot\Api;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,7 +15,39 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+
+    $response = Telegram::getMe();
+    $botId = $response->getId();
+    $firstName = $response->getFirstName();
+    $username = $response->getUsername();
+    echo $botId . '<br />' . $firstName . '<br />' . $username;
 });
 
-Route::resource('users', UserController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/users', [UserController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('users.index');
+Route::get('/users/create', [UserController::class, 'create'])
+    ->middleware(['auth'])
+    ->name('users.create');
+Route::get('/users/{id}', [UserController::class, 'show'])
+    ->middleware(['auth'])
+    ->name('users.show');
+Route::post('/users', [UserController::class, 'store'])
+    ->middleware(['auth'])
+    ->name('users.store');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])
+    ->middleware(['auth'])
+    ->name('users.edit');
+Route::put('/users/{user}', [UserController::class, 'update'])
+    ->middleware(['auth'])
+    ->name('users.update');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])
+    ->middleware(['auth'])
+    ->name('users.destroy');
+
+require __DIR__.'/auth.php';
